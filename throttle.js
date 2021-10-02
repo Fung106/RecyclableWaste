3d60b6cfd23c6d04312271fef6872acc5b5cbdc5
Fung106 <1063115315@qq.com>
2021-10-02 19:42:56 +0800

    添加到远程- js代码

---
+++ throttle.js
@@ -0,0 +1,90 @@
+"use strict"
+
+// let user = {
+//   name : "john",
+//   f(...str){
+//     console.log(this.name);
+//     console.log(...str);
+//   }
+// }
+
+// let admin = {
+//   name : "Pete",
+//   f(...str){
+//     console.log(this.name);
+//     console.log(...str);
+//   }
+// }
+
+// function throttle(func, ms) {
+
+//   let isThrottled = false,
+//     savedArgs,
+//     savedThis;
+
+//   function wrapper() {
+
+//     if (isThrottled) { // (2)
+//       savedArgs = arguments;
+//       savedThis = this;
+//       return;
+//     }
+
+//     func.apply(this, arguments); // (1)
+
+//     isThrottled = true;
+
+//     setTimeout(function() {
+//       isThrottled = false; // (3)
+//       if (savedArgs) {
+//         wrapper.apply(savedThis, savedArgs);
+//         savedArgs = savedThis = null;
+//       }
+//     }, ms);
+//   }
+
+//   return wrapper;
+// }
+
+user.f = throttle(user.f, 1000);
+admin.f = throttle(admin.f, 1000);
+
+
+function throttle(fx, ms){
+  let t = null;
+  let count = 0;
+  let cache = new Map();
+  function time(...str){
+    let savedThis = this;
+    function f1(){
+      fx.apply(savedThis,str);
+    }
+    function stop(){
+      count = 0;
+      cache.get("f")();
+    }
+
+    cache.set("f", f1);
+
+    if(count === 0){
+      count = 1;
+      cache.get("f")();
+      t = setTimeout(stop, ms);
+    }
+  }
+  return time;
+}
+
+user.f = throttle(user.f, 1000);
+admin.f = throttle(admin.f, 1000);
+
+user.f(1);
+admin.f(2);
+user.f(2);
+admin.f(2);
+user.f(3);
+admin.f(4);
+user.f(5);
+admin.f(6);
+
+
